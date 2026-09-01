478b4fda4eb5723b5e2976d1eff0cea96970ba6d
govarthini-19 <govarthinitamilalagan@gmail.com>
2026-09-01 13:30:31 +0530

    Implement create_tables function for Users table
    Add function to create Users table in the database

--- web_app.py
+++ web_app.py
@@ -2,3 +2,15 @@ import streamlit as st
 import sqlite3
 def connect():
     return sqlite3.connect("hiring.db")
+def create_tables():
+    con = connect()
+    cur = con.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS Users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT,
+        role TEXT
+    )
+    """)
